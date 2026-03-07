fa5dcdd0a134a0dd2bbbe87be8d71c609cd1562c
AcrimoniousMirth <samuel_maybury@yahoo.co.uk>
2026-03-07 23:03:19 +0000

    Update sensor status to return full dictionary for UI compatibility

--- extras/mmu_sensors.py
+++ extras/mmu_sensors.py
@@ -748,7 +748,7 @@ class MmuSensors:
 
     def get_status(self, eventtime):
         return {
-            name: bool(sensor.runout_helper.filament_present) if sensor.runout_helper.sensor_enabled else None
+            name: sensor.runout_helper.get_status(eventtime)
             for name, sensor in self.sensors.items()
         }
 
