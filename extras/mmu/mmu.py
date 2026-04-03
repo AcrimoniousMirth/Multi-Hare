b9182794a866a5dc2de39809e7b8e2caf21041af
AcrimoniousMirth <samuel_maybury@yahoo.co.uk>
2026-04-03 10:59:35 +0100

    Fix AttributeError with self.toolhead_name in cmd_MMU_CHANGE_TOOL

--- extras/mmu/mmu.py
+++ extras/mmu/mmu.py
@@ -7175,7 +7175,7 @@ class Mmu:
                                         # Multi-Hare: For System 0 AND when changing systems, skip unload to gate.
                                         # Filament remains in toolhead (at retracted parking distance) while idle.
                                         if new_sys_id != self.system_active and self.sensor_manager.check_all_sensors_in_path():
-                                            self.log_info("Toolhead Swap: Skipping unload to gate, filament will remain mounted in %s" % self.toolhead_name)
+                                            self.log_info("Toolhead Swap: Skipping unload to gate, filament will remain mounted in %s" % self.mmu_toolhead.toolhead_name)
                                         elif self.system_active == 0 and self.sensor_manager.check_all_sensors_in_path():
                                             self.log_info("System 0: Skipping unload to gate, filament will remain in toolhead")
                                         else:
